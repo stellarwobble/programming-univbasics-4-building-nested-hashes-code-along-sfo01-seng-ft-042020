f701d8d4c3798d0d26f6e5efc1a62260b5787fe3
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-24 01:14:47 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,15 +1,6 @@
 def first_challenge
       epic_tragedy ={
-        :montague => {
-          :patriarch => {
-            :name => "Lord Montague",
-            :age => "53"
-          },
-          :matriarch => {
-            :name => "Lady Montague",
-            :age => ""
-          }
-        },
+        :montague => {}
         :capulet => {}
       }
-end
+    end
