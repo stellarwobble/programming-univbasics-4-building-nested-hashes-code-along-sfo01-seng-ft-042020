5825363d344fef5a3c9dd8131443966289eb9e27
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-24 01:06:02 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,49 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {                                                                        :montague => {
+        :patriarch =>{
+          :name =>"Lord Montague",
+          :age => "53"
+        
+       },
+        :matriarch => {
+          :name => "Lady Montague",
+          :age => "54"
+        },
+        :hero_friends => [
+          {
+            :name => "Benvolio",
+            :age => "17",
+            :attitude => "hot-headed"
+          }
+          ]
+   },
+   :capulet => {
+     :patriarch{
+       :name => "Lord Capulet",
+       :age => "50"
+     },
+     :matriarch => {
+       :name => "Lady Capulet",
+       :age => "51"
+     },
+     :heroine => {
+       :name => "Juliet",
+       :age => "15",
+       :status => "alive"
+     },
+     :heroine_friends => [
+       {
+         :name => "Steven",
+         :age => "30",
+         :attitude => "confused"
+       },
+       {
+         :name => "Nurse",
+         :age => "44",
+         :attitude => "worried"
+       }
+       ]
+   }
+     
+   }
 end
