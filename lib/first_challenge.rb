81092988bd6945b30d14f04b8b9e4586da16d3e6
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-24 01:07:34 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,11 +4,11 @@ def first_challenge
           :name =>"Lord Montague",
           :age => "53"
         
-       },
+       } ,
         :matriarch => {
           :name => "Lady Montague",
           :age => "54"
-        },
+        } ,
         :hero_friends => [
           {
             :name => "Benvolio",
@@ -16,27 +16,27 @@ def first_challenge
             :attitude => "hot-headed"
           }
           ]
-   },
+   } ,
    :capulet => {
      :patriarch {
        :name => "Lord Capulet",
        :age => "50"
-     },
+     } ,
      :matriarch => {
        :name => "Lady Capulet",
        :age => "51"
-     },
+     } ,
      :heroine => {
        :name => "Juliet",
        :age => "15",
        :status => "alive"
-     },
+     } ,
      :heroine_friends => [
        {
          :name => "Steven",
          :age => "30",
          :attitude => "confused"
-       },
+       } ,
        {
          :name => "Nurse",
          :age => "44",
